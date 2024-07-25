7730065f6aa6a503e00c8d38366bdc4c65ce7cd9
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-07-25 22:03:00 +0300

    add app colors and themes classes

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const darkBackground = Color(0xff0D0C0C);
+  static const lightBackground = Color(0xffF2F2F2);
+  static const primaryColor = Color(0xff42C83C);
+  static const navBar = Color(0xff343434);
+  static const navBarIcons = Color(0xff808080);
+  static const grey = Color(0xff616161);
+}
