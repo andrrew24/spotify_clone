0a53d4db351c9ac0d8ada8698a65aa25cd226687
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-07-26 03:03:35 +0300

    add assets and fonts

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -3,38 +3,28 @@ import 'package:spotify_clone/core/config/theme/app_colors.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
-    brightness: Brightness.light,
-    primaryColor: AppColors.primaryColor,
-    scaffoldBackgroundColor: AppColors.lightBackground,
-    elevatedButtonTheme:ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.primaryColor,
-        textStyle:const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
-        )
-      )
-    ) 
-  );
+      brightness: Brightness.light,
+      fontFamily: 'Satoshi',
+      primaryColor: AppColors.primaryColor,
+      scaffoldBackgroundColor: AppColors.lightBackground,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primaryColor,
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
 
   static final darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    primaryColor: AppColors.primaryColor,
-    scaffoldBackgroundColor: AppColors.darkBackground,
-    elevatedButtonTheme:ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.primaryColor,
-        textStyle:const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold
-        ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
-        )
-      )
-    ) 
-  );
+      brightness: Brightness.dark,
+      fontFamily: 'Satoshi',
+      primaryColor: AppColors.primaryColor,
+      scaffoldBackgroundColor: AppColors.darkBackground,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.primaryColor,
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)))));
 }
