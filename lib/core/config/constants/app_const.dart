0bbe4420ee0bed391f6f73358ff8aba8405d7771
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-09-23 17:38:23 +0300

    used pocketbase auth store to handle user session

--- lib/core/config/constants/app_const.dart
+++ lib/core/config/constants/app_const.dart
@@ -1,6 +1,8 @@
-class AppConstants {
-  static const String accessToken = "access_token";
-  static const String localHost = "http://10.0.2.2:8090";
+abstract class AppConstants {
+  static const String kAuth = "auth";
+  static const String kToken = "token";
+  static const String kId = "id";
+  static const String baseUrl = "http://10.0.2.2:8090";
   static const String songCollectionId = "2tw7h5a5q8bjqyj";
-  static const String appApi = "$localHost/api/files/$songCollectionId/";
+  static const String appApi = "$baseUrl/api/files/$songCollectionId/";
 }
